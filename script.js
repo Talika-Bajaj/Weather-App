4cb7566d31ed51c6547340272cbe40ef23ad95ad
Talika Bajaj <bajajtalika2001@gmail.com>
2024-03-27 14:04:40 +0530

    Check files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // import api-key from "./data.json";
-import key from './data.json';
+// import key from './data.json';
 
 let city = document.getElementById("city");
 let searchBtn = document.getElementById("search-btn");
